cdfc672b410f14a52daf849eaaaf9a9d2d724f34
Sourav Mohile <souravmohile@icloud.com>
2024-07-13 16:42:55 +0100

    Feature for deleting all entries & visiting individual entries for updating/deletin

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blogpost/", views.BlogPostListCreate.as_view(), name="blogpost-view-create")
+    path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+    path("blogposts/<int:pk>/", views.BlogPostRetrieveUpdateDestroy.as_view(), name="update")
 ]
