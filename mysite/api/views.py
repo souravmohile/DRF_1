61d0ca5b8f5a07e4c9188ffad25ef8bcca14f5d6
Sourav Mohile <souravmohile@icloud.com>
2024-07-13 17:01:35 +0100

    Querying based on the name of an object

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -18,3 +19,16 @@ class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "pk"
+
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+
+        title = request.query_params.get("title", "")
+
+        if title:
+            blog_posts = BlogPost.objects.filter(title_icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
